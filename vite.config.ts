325d40aab668fb5a13d09defef8339c9bc0f1f55
liuziting <liuziting2014@163.com>
2025-07-22 14:02:06 +0800

    优化部署错误

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
-  }
+    plugins: [vue()],
+    server: {
+        port: 3000,
+        open: true
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        minify: 'terser',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['vue'],
+                    utils: ['@vueuse/core']
+                }
+            }
+        }
+    },
+    base: './'
 })
